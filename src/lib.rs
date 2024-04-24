4fdea7a74caca32da09581a0a48097060796cc6a
Jonathan Baker <jonathan.baker@fsgi.com>
2024-04-24 10:34:09 -0500

    parse procedure

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,27 @@
+use winnow::combinator::*;
+use winnow::token::any;
+use winnow::PResult;
+use winnow::Parser;
+
+use crate::combinators::*;
+use crate::tokens::*;
+
 mod combinators;
-mod tokens;
+pub mod tokens;
+
+pub fn parse_input<'a>(input: &mut &'a str) -> PResult<Statement<'a>> {
+    trim(dispatch! {peek(any);
+    '/' => parse_comment,
+    '{' => parse_include,
+    _ => skip_to_comment_or_include
+    })
+    .parse_next(input)
+}
+
+pub fn parse_procedure<'a>(input: &'a str) -> PResult<Vec<Statement<'a>>> {
+    let mut iter = iterator(input, parse_input);
+    let statements = iter.collect::<Vec<Statement>>();
+    let _result = iter.finish();
+
+    Ok(statements)
+}
