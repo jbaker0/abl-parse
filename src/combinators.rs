c09d9e8ea45c58c3ba9496f57ff5bdcaa50f4138
Jonathan Baker <jonathan.baker@fsgi.com>
2024-04-24 10:33:58 -0500

    handle comments

--- src/combinators.rs
+++ src/combinators.rs
@@ -1,17 +1,18 @@
 use winnow::ascii::{alpha1, multispace0, multispace1};
-use winnow::combinator::{delimited, separated, trace};
-use winnow::error::{ContextError, ErrMode, InputError, ParserError};
+use winnow::combinator::{alt, delimited, repeat_till, separated};
+use winnow::error::ParserError;
 use winnow::seq;
-use winnow::token::{take_until, take_while};
-use winnow::{IResult, PResult, Parser};
+use winnow::token::{any, take_until, take_while};
+use winnow::{PResult, Parser};
 
-use crate::tokens;
 use crate::tokens::Include;
+use crate::Statement;
 
 static LEFT_GULLWING: char = '{';
 static RIGHT_GULLWING: char = '}';
-static FULL_STOP: char = '.';
-static SPACE: char = ' ';
+
+static OPEN_COMMENT: &str = "/*";
+static CLOSE_COMMENT: &str = "*/";
 
 pub(crate) fn trim<'a, F, O, E>(inner: F) -> impl Parser<&'a str, O, E>
 where
@@ -21,7 +22,7 @@ where
     delimited(multispace0, inner, multispace0)
 }
 
-pub(crate) fn parse_between_gullwings<'a>(input: &mut &'a str) -> PResult<&'a str> {
+pub(crate) fn between_gullwings<'a>(input: &mut &'a str) -> PResult<&'a str> {
     delimited(
         LEFT_GULLWING,
         take_until(0.., RIGHT_GULLWING),
@@ -30,7 +31,7 @@ pub(crate) fn parse_between_gullwings<'a>(input: &mut &'a str) -> PResult<&'a st
     .parse_next(input)
 }
 
-pub(crate) fn parse_not_single_digit<'a>(input: &mut &'a str) -> PResult<&'a str> {
+pub(crate) fn not_single_digit<'a>(input: &mut &'a str) -> PResult<&'a str> {
     if input.len() == 1 {
         alpha1.parse_next(input)
     } else {
@@ -39,35 +40,58 @@ pub(crate) fn parse_not_single_digit<'a>(input: &mut &'a str) -> PResult<&'a str
 }
 
 pub(crate) fn parse_include_path<'a>(input: &mut &'a str) -> PResult<&'a str> {
-    // Should use multispace1 instead of SPACE at some point
-    trim(take_while(1.., |c| c != SPACE && c != RIGHT_GULLWING )).parse_next(input)
+    take_while(1.., |c: char| !c.is_whitespace() && c != RIGHT_GULLWING).parse_next(input)
 }
 
 // This fails on leading whitespace, may need to fix
+// Grabs comments inside the include as arguments, needs fix
 pub(crate) fn parse_include_args<'a>(input: &mut &'a str) -> PResult<Vec<&'a str>> {
-    let args = separated(0.., 
-        take_while(0.., |c: char| !c.is_whitespace() ),
-        multispace1
+    let args = separated(
+        0..,
+        take_while(0.., |c: char| !c.is_whitespace()),
+        multispace1,
     )
     .parse_next(input)?;
 
     Ok(args)
 }
 
-pub(crate) fn parse_include<'a>(input: &mut &'a str) -> PResult<Include<'a>> {
-    let mut between = parse_between_gullwings(input)?;
+pub(crate) fn parse_include<'a>(input: &mut &'a str) -> PResult<Statement<'a>> {
+    let mut between = between_gullwings(input)?;
+    ("Between: {}", between);
 
-    let (path, args) = seq!(
-        parse_include_path,
-        parse_include_args,
-    ).parse_next(&mut between)?;
+    // Hacky way of keeping ABL params from being parsed as includes
+    // Need to revisit this later
+    if between.len() == 1 && between.chars().next().unwrap().is_digit(10) {
+        return Ok(Statement::Comment(between));
+    }
 
-    Ok(Include {
+    let (path, args) = seq!(parse_include_path, parse_include_args,).parse_next(&mut between)?;
+
+    Ok(Statement::Include(Include {
         path,
         content: None,
         arguments: args,
-    })
-    
+    }))
+}
+
+pub(crate) fn skip_to_comment_or_include<'a>(input: &mut &'a str) -> PResult<Statement<'a>> {
+    let (_skipped, statement): (Vec<char>, Statement<'a>) =
+        repeat_till(0.., any, alt((parse_comment, parse_include))).parse_next(input)?;
+
+    return Ok(statement);
+}
+
+pub(crate) fn parse_comment<'a>(input: &mut &'a str) -> PResult<Statement<'a>> {
+    let comment =
+        delimited(OPEN_COMMENT, take_until(0.., CLOSE_COMMENT), CLOSE_COMMENT).parse_next(input)?;
+
+    let count = comment.matches(OPEN_COMMENT).count();
+    for _ in 0..count {
+        take_until(0.., CLOSE_COMMENT).parse_next(input)?;
+    }
+
+    Ok(Statement::Comment(comment))
 }
 
 #[cfg(test)]
@@ -76,34 +100,22 @@ mod tests {
 
     #[test]
     fn test_parse_between_gullwings() {
+        assert_eq!(between_gullwings(&mut "{ include.i }"), Ok(" include.i "));
+        assert_eq!(between_gullwings(&mut "{include.i}"), Ok("include.i"));
         assert_eq!(
-            parse_between_gullwings(&mut "{ include.i }"),
-            Ok(" include.i ")
-        );
-        assert_eq!(
-            parse_between_gullwings(&mut "{include.i}"),
-            Ok("include.i")
-        );
-        assert_eq!(
-            parse_between_gullwings(&mut "{ include.i &val1 &val2 &val3 }"),
+            between_gullwings(&mut "{ include.i &val1 &val2 &val3 }"),
             Ok(" include.i &val1 &val2 &val3 ")
         );
         assert_eq!(
-            parse_between_gullwings(&mut "{include.i &val1 &val2 &val3}"),
+            between_gullwings(&mut "{include.i &val1 &val2 &val3}"),
             Ok("include.i &val1 &val2 &val3")
         );
     }
 
     #[test]
     fn test_parse_include_path() {
-        assert_eq!(
-            parse_include_path(&mut "include.i a"),
-            Ok("include.i")
-        );
-        assert_eq!(
-            parse_include_path(&mut " include.i a"),
-            Ok("include.i")
-        );
+        assert_eq!(parse_include_path(&mut "include.i a"), Ok("include.i"));
+        assert_eq!(parse_include_path(&mut " include.i a"), Ok("include.i"));
         assert_eq!(
             parse_include_path(&mut "include.i &val1 &val2 &val3"),
             Ok("include.i")
@@ -124,29 +136,26 @@ mod tests {
             parse_include_args(&mut "val1 val2 val3 "),
             Ok(vec!["val1", "val2", "val3", ""])
         );
-        assert_eq!(
-            parse_include_args(&mut "1 2 3"),
-            Ok(vec!["1", "2", "3"])
-        );
+        assert_eq!(parse_include_args(&mut "1 2 3"), Ok(vec!["1", "2", "3"]));
     }
 
     #[test]
     fn test_parse_include() {
         assert_eq!(
-            parse_include(&mut "{include.i}"),
-            Ok(Include {
+            parse_include(&mut "{ include.i }"),
+            Ok(Statement::Include(Include {
                 path: "include.i",
                 content: None,
                 arguments: vec![""],
-            })
+            }))
         );
         assert_eq!(
-            parse_include(&mut "{include.i &val1 &val2 &val3}"),
-            Ok(Include {
+            parse_include(&mut "{ include.i &val1 &val2 &val3 }"),
+            Ok(Statement::Include(Include {
                 path: "include.i",
                 content: None,
-                arguments: vec!["&val1", "&val2", "&val3"],
-            })
+                arguments: vec!["&val1", "&val2", "&val3", ""],
+            }))
         );
     }
 }
